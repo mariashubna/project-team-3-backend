0a3777dfc50b108581295a4648aa37027f35b358
Sergii <kryvkosergii@gmail.com>
2025-07-09 11:13:25 +0300

    #34 * public endpoint - get all recipes by filter

--- swagger.js
+++ swagger.js
@@ -5,55 +5,116 @@ const BASE_URL = process.env.APP_URL || `http://localhost:3000`;
 console.log(`Swagger URL: ${BASE_URL}/api-docs`);
 
 const swaggerOptions = {
-    definition: {
-        openapi: "3.0.0",
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: "http",
-                    scheme: "bearer",
-                    bearerFormat: "JWT",
-                },
-            },
+  definition: {
+    openapi: "3.0.0",
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
-        info: {
-            title: "Foodies API",
-            version: "1.0.0",
-            description: "Foodies application API documentation",
+      },
+      schemas: {
+        User: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            avatar: { type: "string" },
+            name: { type: "string" },
+            email: { type: "string", format: "email" },
+          },
         },
-        servers: [
-            {
-                url: BASE_URL,
+        Category: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            name: { type: "string" },
+          },
+        },
+        Area: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            name: { type: "string" },
+          },
+        },
+        Ingredient: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            name: { type: "string" },
+            description: { type: "string" },
+            image: { type: "string" },
+          },
+        },
+        IngredientMeasurement: {
+          type: "object",
+          properties: {
+            ingredient: { $ref: "#/components/schemas/Ingredient" },
+            measure: { type: "string" },
+          },
+        },
+        Recipe: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            title: { type: "string" },
+            category: {
+              $ref: "#/components/schemas/Category",
             },
-        ],
-        tags: [
-            {
-                name: "Users",
-                description: "Manage users",
+            instructions: { type: "string" },
+            description: { type: "string" },
+            image: { type: "string" },
+            time: { type: "string" },
+            owner: { $ref: "#/components/schemas/User" },
+            ingredients: {
+              type: "array",
+              items: { $ref: "#/components/schemas/IngredientMeasurement" },
             },
-            {
-                name: "Categories",
-                description: "Manage categories",
-            },
-            {
-                name: "Areas",
-                description: "Manage areas",
-            },
-            {
-                name: "Ingredients",
-                description: "Manage ingredients",
-            },
-            {
-                name: "Recipes",
-                description: "Manage recipes",
-            },
-            {
-                name: "Testimonials",
-                description: "Manage testimonials",
-            },
-        ],
+            area: { $ref: "#/components/schemas/Area" },
+          },
+        },
+      },
     },
-    apis: ["./routes/*.js"],
+    info: {
+      title: "Foodies API",
+      version: "1.0.0",
+      description: "Foodies application API documentation",
+    },
+    servers: [
+      {
+        url: BASE_URL,
+      },
+    ],
+    tags: [
+      {
+        name: "Users",
+        description: "Manage users",
+      },
+      {
+        name: "Categories",
+        description: "Manage categories",
+      },
+      {
+        name: "Areas",
+        description: "Manage areas",
+      },
+      {
+        name: "Ingredients",
+        description: "Manage ingredients",
+      },
+      {
+        name: "Recipes",
+        description: "Manage recipes",
+      },
+      {
+        name: "Testimonials",
+        description: "Manage testimonials",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 
 export const specs = swaggerJsdoc(swaggerOptions);
